2130f293b6d679a7801098b5df378f642960e21b
genify <genify@163.com>
2013-05-27 10:41:00 +0800

    history for ie8-

--- src/patched/trident-0/api.js
+++ src/patched/trident-0/api.js
@@ -114,52 +114,6 @@ var f = function(){
         var _mask = _cache[_event.args[0].msk];
         if (!!_mask) _e._$removeByEC(_mask);
     });
-    /**
-     * 将Hash推入历史
-     * @param  {String} _hash HASH值
-     * @return {Void}
-     */
-    _h.__pushHistory = (function(){
-        var _timer,
-            _iframe,
-            _queue = [],
-            _hflag = 'cb-'+(+new Date),
-            _content = '<script>parent.nej.h["'+_hflag+'"] = !0;parent.location.hash = decodeURIComponent("#<HASH>");</scr'+'ipt>';
-        var _doCheckQueue = function(){
-            _timer = window.clearTimeout(_timer);
-            if (!_queue.length) return;
-            var _hash = _queue.shift();
-            try{
-                var _document = _iframe.contentWindow.document;
-                _document.open();
-                _document.write('<head><title>');
-                _document.write(document.title);
-                _document.write('</title>');
-                _document.write(_content.replace('#<HASH>'
-                               ,encodeURIComponent(_hash)));
-                _document.write('</head>');
-                if (location.hostname!=document.domain)
-                    _document.domain = document.domain;
-                _document.close();
-                _h[_hflag] = !1;
-            }catch(ex){
-                alert(ex.message||ex);
-                _queue.unshift(_hash);
-            }
-            _timer = window.setTimeout(_doCheckQueue,50);
-        };
-        return _h.__pushHistory._$aop(
-               function(_event){
-                   _event.stopped = !0;
-                   var _hash = _event.args[0];
-                   if (!!_h[_hflag]||
-                      (!_iframe&&!_hash)) return;
-                   _queue.push(_hash);
-                   if (!_iframe)
-                       _iframe = _e._$createXFrame();
-                   _doCheckQueue();
-               });
-    })();
 };
 NEJ.define('{lib}patched/trident-0/api.js',
       ['{lib}patched/api.js'],f);
--- src/patched/trident/api.js
+++ src/patched/trident/api.js
@@ -395,6 +395,55 @@ var f = function(){
             _event.value==2147483647)
             _event.value = '';
     });
+// fix for ie6-ie8
+if (_p._$KERNEL.release<5.0){
+    /**
+     * 将Hash推入历史
+     * @param  {String} _hash HASH值
+     * @return {Void}
+     */
+    _h.__pushHistory = (function(){
+        var _timer,
+            _iframe,
+            _queue = [],
+            _hflag = 'cb-'+(+new Date),
+            _content = '<script>parent.nej.h["'+_hflag+'"] = !0;parent.location.hash = decodeURIComponent("#<HASH>");</scr'+'ipt>';
+        var _doCheckQueue = function(){
+            _timer = window.clearTimeout(_timer);
+            if (!_queue.length) return;
+            var _hash = _queue.shift();
+            try{
+                var _document = _iframe.contentWindow.document;
+                _document.open();
+                _document.write('<head><title>');
+                _document.write(document.title);
+                _document.write('</title>');
+                _document.write(_content.replace('#<HASH>'
+                               ,encodeURIComponent(_hash)));
+                _document.write('</head><body></body>');
+                if (location.hostname!=document.domain)
+                    _document.domain = document.domain;
+                _document.close();
+                _h[_hflag] = !1;
+            }catch(ex){
+                console.log(ex.message||ex);
+                _queue.unshift(_hash);
+            }
+            _timer = window.setTimeout(_doCheckQueue,50);
+        };
+        return _h.__pushHistory._$aop(
+               function(_event){
+                   _event.stopped = !0;
+                   var _hash = _event.args[0];
+                   if (!!_h[_hflag]||
+                      (!_iframe&&!_hash)) return;
+                   _queue.push(_hash);
+                   if (!_iframe)
+                       _iframe = _e._$createXFrame();
+                   _doCheckQueue();
+               });
+    })();
+}
     // cache background image
     try{document.execCommand('BackgroundImageCache',!1,!0);}catch(e){}
 };
