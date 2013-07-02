9cfe9eab7970578a527039f2876251aca825d999
genify <genify@163.com>
2013-07-02 16:33:43 +0800

    cef player

--- src/native/cef/util/player.js
+++ src/native/cef/util/player.js
@@ -171,9 +171,6 @@ var f = function() {
     _proPlayer._$play = function(_options){
         _n._$exec('player.load',_options);
         _n._$exec('player.setInfo',_options);
-        this._$dispatchEvent('onstatechange',{
-            state:0
-        });
     };
     /**
      * 恢复状态
