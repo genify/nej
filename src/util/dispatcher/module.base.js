8264304e3b2b710893a15eba10ad31640fac9da3
genify <genify@163.com>
2013-07-27 15:23:50 +0800

    delay for dispatcher composite

--- src/util/dispatcher/module.base.js
+++ src/util/dispatcher/module.base.js
@@ -84,6 +84,16 @@ var f = function(){
         _e._$removeByEC(this.__body);
     };
     /**
+     * 是否忽略模块组合
+     * @return {Boolean} 是否忽略
+     */
+    _proAbstractModule.__isEscapedComposite = (function(){
+        var _reg0 = /^onshow|onrefresh|delay$/;
+        return function(_umi){
+            return _reg0.test(_umi);    
+        }
+    })();
+    /**
      * 生成组合模块的输入参数，子类实现具体业务逻辑
      * @param  {String} 模块类别
      * @param  {Object} 输入信息
@@ -97,9 +107,8 @@ var f = function(){
      * @return {Void}
      */
     _proAbstractModule.__doApplyComposite = (function(){
-        var _reg0 = /^onshow|onrefresh|delay$/;
         var _doRedirect = function(_query,_options,_umi,_pid){
-            if (_reg0.test(_pid)) return;
+            if (this.__isEscapedComposite(_pid)) return;
             if (!!_query) _umi += (_umi.indexOf('?')>1?'&':'?')+_query;
             var _input = this.__doGenCompositeParam(_pid,_options)||{};
             _input.location = _options;
@@ -107,11 +116,13 @@ var f = function(){
             this.__dispatcher._$redirect(_umi,{input:_input});
         };
         return function(_type,_options){
-            // delay all
-            if (!!this.__composites.delay) return;
-            // check delay
-            var _composite = this.__composites[_type];
-            if (_composite.delay) return;
+            if (!_options.nodelay){
+                // delay all
+                if (!!this.__composites.delay) return;
+                // check delay
+                var _composite = this.__composites[_type]||_o;
+                if (_composite.delay) return;
+            }
             // do apply
             var _query = _u._$object2query(_options.param)||'';
             if (_type=='onrefresh'){
@@ -131,8 +142,9 @@ var f = function(){
      * @return {Void}
      */
     _proAbstractModule.__doHideComposite = (function(){
-        var _doHide = function(_umi){
-            this.__dispatcher._$hide(_umi);
+        var _doHide = function(_umi,_pid){
+            if (!this.__isEscapedComposite(_pid))
+                 this.__dispatcher._$hide(_umi);
         };
         return function(){
             _u._$forIn(this.__composites,_doHide,this);
