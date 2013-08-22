2f5d372dea27ce977b0b1014b4f83f94907cc1ce
java0racle <java0racle@163.com>
2013-08-22 18:13:48 +0800

    effect

--- src/patched/trident/effect.js
+++ src/patched/trident/effect.js
@@ -42,87 +42,89 @@ var f = function(){
     	// 适配特殊属性
     	var _doAdap  = function(_value,_prop){
     		if(_prop === 'filter'){
-                _value = _u._$fixed(_value,0);
-                _value = 'alpha(opacity=' + _value + ')';
-            }
-            if(_prop === 'z-index')
-                _value = _u._$fixed(_value,0);
-           	return _value;
+          _value = _u._$fixed(_value,0);
+          _value = 'alpha(opacity=' + _value + ')';
+        }
+        if(_prop === 'z-index')
+          _value = _u._$fixed(_value,0);
+       	return _value;
     	};
     	// 适配构造器
-    	var _doFindCutr = function(_value){
+    	var _doFindCur = function(_value){
     		return _animMap[_value.split(' ')[2]];
     	};
     	// 解析参数
-		var _doParse = function(_node,_value,_rules,_stop,_index){
-			var _value= _value.split(' '),
-				_prop = _value[0],
-				_from = parseFloat(_e._$getStyle(_node,_prop))||0,
-				_to   = parseFloat(_rules[_prop])||0,
-				_cutr = _animMap[_value[2]],
-				_durt = _value[1].slice(0,-1) * 1000 + _value[3].slice(0,1) * 1000;
-			if(_durt > _node._sumtime){
-				_node.isLastOne = _index;
-			}
-			// IE6-8
-			if(_prop === 'opacity' && (nej.p._$KERNEL.release - 5) < 0){
-				_prop = 'filter';
-				var _filter = _e._$getStyle(_node,_prop);
-				_from = parseFloat(_filter.split('=')[1])||0;
-				_to   = _to * 100;
-			}
-			var _options = {
-			    from:{
-		            offset:_from
-		        },
-		        to:{
-		            offset:_to
-		        },
-		        duration:_durt,
-		        onupdate:function(_offset){
-			        var _value = _offset.offset;
-			        if(!_h.__doCheckProp(_prop)){
-			        	_value = _doAdap(_value,_prop)
-		                _e._$setStyle(_node,_prop,_value);
-		            }else
-		            	_e._$setStyle(_node,_prop,_value + 'px');
-		        },
-		        onstop:function(_prop){
-		            var _effect = _node.effects[_index];
-		            	_effect = _cutr._$recycle(_effect);
-		            if(_node.isLastOne === _index)
-		            	_stop.apply(this);
-		        }._$bind(this,_index)
-			};
-			return _options;
-		};
-		return _h.__onStart._$aop(function(_event){
-			_event.stopped = !0;
-			var _list = _event.args;
-			var _node = _list[0],
-				_rules= _list[1],
-				_anim = _list[2],
-				_stop = _list[3];
-			_node._sumtime = 0,_node.isLastOne = 0;
-			var _effects   = [];
-			if(_anim.indexOf('all') > -1)
-				_anim = _doRbAnim(_rules,_anim);
-			var _animArray = _anim.split(',');
-			_node.effects  = [];
-			_u._$forEach(_animArray,function(_value,_index){
-				if(_value === '')
-					return false;
-			  	var _options = _doParse(_node,_value,_rules,_stop,_index);
-			  	_effects.push({o:_options,c:_doFindCutr(_value)});
-			  	_node.effects.push(0);
-			});
-			_u._$forEach(_effects,function(_item,_index){
-				var _effect = _item.c._$allocate(_item.o);
-				_node.effects[_index] = _effect;
-				_effect._$play();
-			});
-			return this;
-		});
+		  var _doParse = function(_node,_value,_rules,_stop,_index){
+  			var _value= _value.split(' '),
+    				_prop = _value[0],
+    				_from = parseFloat(_e._$getStyle(_node,_prop))||0,
+    				_to   = parseFloat(_rules[_prop])||0,
+    				_cutr = _animMap[_value[2]],
+    				_durt = _value[1].slice(0,-1) * 1000 + _value[3].slice(0,1) * 1000;
+  			if(_durt >= _node.sumTime){
+          _node.sumTime = _durt;
+  				_node.isLastOne = _index;
+  			}
+  			// IE6-8
+        var _isLowerIE = (nej.p._$KERNEL.engine === 'trident' && (nej.p._$KERNEL.release - 5) < 0);
+  			if(_prop === 'opacity' && _isLowerIE){
+  				_prop = 'filter';
+  				var _filter = _e._$getStyle(_node,_prop);
+  				_from = parseFloat(_filter.split('=')[1])||0;
+  				_to   = _to * 100;
+  			}
+  			var _options = {
+    	      from:{
+                offset:_from
+            },
+            to:{
+                offset:_to
+            },
+            duration:_durt,
+            onupdate:function(_offset){
+    	        var _value = _offset.offset;
+    	        if(!_h.__doCheckProp(_prop)){
+    	        	_value = _doAdap(_value,_prop)
+                _e._$setStyle(_node,_prop,_value);
+              }else{
+                _e._$setStyle(_node,_prop,_value + 'px');
+              }
+            },
+            onstop:function(_prop){
+                var _effect = _node.effects[_index];
+                if(!_effect) return;
+                	  _effect = _cutr._$recycle(_effect);
+                if(_node.isLastOne === _index)
+                	_stop.apply(this);
+            }._$bind(this,_index)
+    			};
+  			return _options;
+  		};
+		  return _h.__onStart._$aop(function(_event){
+    		_event.stopped = !0;
+    		var _list = _event.args;
+    		var _node = _list[0],
+    			  _rules= _list[1],
+    			  _anim = _list[2],
+    			  _stop = _list[3];
+    		_node.sumTime = 0,_node.isLastOne = 0;
+    		var _effects   = [];
+    		if(_anim.indexOf('all') > -1)
+    			_anim = _doRbAnim(_rules,_anim);
+    		var _animArray = _anim.slice(0,-1);
+            _animArray = _animArray.split(',');
+    		_node.effects  = [];
+    		_u._$forEach(_animArray,function(_value,_index){
+    		  	var _options = _doParse(_node,_value,_rules,_stop,_index);
+    		  	_effects.push({o:_options,c:_doFindCur(_value)});
+    		});
+    		_u._$forEach(_effects,function(_item,_index){
+    			var _effect = _item.c._$allocate(_item.o);
+    			_node.effects[_index] = _effect;
+    			_effect._$play();
+    		});
+    		return this;
+    	});
     })(); 
 
     /**
@@ -135,10 +137,11 @@ var f = function(){
     	_event.stopped = !0;
     	var _list = _event.args;
     	var _node = _list[0];
-		_u._$forEach(_node.effects,function(_o){
-			_o._$stop();
-		});
-		return this;
+  		_u._$forEach(_node.effects,function(_o){
+  			_o._$stop();
+  		});
+      _node.effects = [];
+  		return this;
     });
 
     /**
