d921bc0def8e35ec0dc9fc73a9cda70b2e5b8303
genify <genify@163.com>
2013-11-21 15:37:59 +0800

    bugfix: fix gen absolute uri in define.js

--- src/define.js
+++ src/define.js
@@ -190,12 +190,20 @@ var _doStr2Obj = function(_query){
  * @return {String}      格式化后地址
  */
 var _doFormatURI = (function(){
-    var _reg = /{(.*?)}/gi,
+    var _xxx = !1,
+        _reg = /{(.*?)}/gi,
         _anchor = d.createElement('a');
     var _absolute = function(_uri){
         return _uri.indexOf('://')>0;
     };
+    var _append = function(){
+        if (_xxx) return;
+        _xxx = !0;
+        _anchor.style.display = 'none';
+        document.body.appendChild(_anchor);
+    };
     return function(_uri){
+        _append();
         if (!_uri) return '';
         if (_absolute(_uri)) return _uri;
         var _uri = _uri.replace(_reg,function($1,$2){
