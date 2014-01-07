98b9b3d07ca48d01cf336a74741462db4dfde4bb
unknown <genif_000@.(none)>
2014-01-07 10:55:14 +0800

    support multi-audio instance for background sound

--- src/patched/audio.js
+++ src/patched/audio.js
@@ -20,13 +20,14 @@ var f = function(){
         return _t._$$MediaAudio._$allocate(_options);
     };
     /**
-     * 单例播放音频，代码示例
+     * 播放音频，代码示例
      * [code]
      *     // 播放音频
      *     // 如果之前有音频在播放
      *     // 则会先触发之前音频的0状态onstatechange事件
      *     nej.e._$playBgSound(
      *         'http://a.b.com/a/a.mp3',{
+     *             key:'test-audio'
      *             extra:'xxx_id',
      *             onstatechange:function(_event){
      *                 // _event.state -> 状态值
@@ -40,100 +41,129 @@ var f = function(){
      *     // 停止音频播放
      *     // 如果有音频在播放
      *     // 则会触发0状态的onstatechange事件
-     *     nej.e._$stopBgSound();
+     *     nej.e._$stopBgSound('test-audio');
      * [/code]
      * @api    {nej.e._$playBgSound}
      * @param  {String} 音频文件地址
      * @param  {Object} 可选配置参数
-     * @config {Variable} extra         onstatechange时传回数据
+     * @config {String}   key           播放标识，同一标识只允许一个播放实例
+     * @config {Variable} extra         onstatechange/onerror时传回数据
      * @config {Number}   retry         出错重试次数，0表示不重试，默认为0
      * @config {Number}   interval      如果设置了retry则通过此参数指定每次重试间隔，单位毫秒，默认500
      * @config {Function} onstatechange 播放状态变化回调事件，state值为
-     *  [ntb]
-     *  0 - 当前停止状态
-     *  1 - 当前缓冲状态
-     *  2 - 当前播放状态
-     *  3 - 当前暂停状态
-     *  [/ntb]
+     *                                  [ntb]
+     *                                   0 | 当前停止状态
+     *                                   1 | 当前缓冲状态
+     *                                   2 | 当前播放状态
+     *                                   3 | 当前暂停状态
+     *                                  [/ntb]
      * @config  {Function} onerror      播放异常回调事件
      * @return {Void}
      */
     _e._$playBgSound = (function(){
-        var _playing,_audio,_timer;
+        // audio player cache
+        // url   - audio url
+        // conf  - play config
+        // audio - audio instance
+        // timer - retry timer
+        var _pcache = {};
         // state change callback
-        var _doStateChangeCallback = function(_state){
-            if (!_playing) return;
-            var _func = _playing.onstatechange||_f,
-                _extr = _playing.extra;
-            if (_state==0) _playing = null;
+        var _doStateChangeCallback = function(_key,_state){
+            var _cch = _pcache[_key];
+            if (!_cch) return;
+            var _conf = _cch.conf,
+                _func = _conf.onstatechange||_f,
+                _extr = _conf.extra;
+            if (_state==0){
+                delete _pcache[_key];
+            }
             _func({state:_state,data:_extr});
         };
         // error callback
-        var _doErrorCallback = function(_code){
-            if (!_playing) return;
-            var _func = _playing.onerror||_f,
-                _extr = _playing.extra;
-            _playing = null;
+        var _doErrorCallback = function(_key,_code){
+            var _cch = _pcache[_key];
+            if (!_cch) return;
+            var _conf = _cch.conf,
+                _func = _conf.onerror||_f,
+                _extr = _conf.extra;
+            delete _pcache[_key];
             _func({code:_code,data:_extr});
         };
         // state change action
-        var _doStateChangeAction = function(_event){
+        var _doStateChangeAction = function(_key,_event){
             if (_event.state==0){
-                _doStopAction();
+                _doClearAction(_key);
             }
-            _doStateChangeCallback(_event.state);
+            _doStateChangeCallback(_key,_event.state);
         };
         // play action
-        var _doPlayAction = function(){
-            if (!_playing) return;
-            _audio = _h.__getAudioInst({
-                url:_playing.url,
-                onerror:_doErrorAction,
-                onstatechange:_doStateChangeAction
+        var _doPlayAction = function(_key){
+            var _cch = _pcache[_key];
+            if (!_cch) return;
+            _cch.audio = _h.__getAudioInst({
+                url:_cch.url,
+                onerror:_doErrorAction._$bind(null,_key),
+                onstatechange:_doStateChangeAction._$bind(null,_key)
             });
-            _audio._$play();
+            _cch.audio._$play();
         };
         // stop action
-        var _doStopAction = function(){
-            if (!!_audio){
-                _audio = _audio._$recycle();
+        var _doClearAction = function(_key){
+            var _cch = _pcache[_key];
+            if (!_cch) return;
+            if (!!_cch.audio){
+                _cch.audio = _cch.audio._$recycle();
             }
-            if (!!_timer){
-                _timer = window.clearTimeout(_timer);
+            if (!!_cch.timer){
+                _cch.timer = window.clearTimeout(_cch.timer);
             }
         };
         // error action
-        var _doErrorAction = function(_event){
-            if (!_playing) return;
-            _doStopAction();
-            if (_playing.retry>0){
-                _playing.retry--;
-                _timer = window.setTimeout(
-                    _doPlayAction._$bind(null),
-                    _playing.interval||500
+        var _doErrorAction = function(_key,_event){
+            var _cch = _pcache[_key];
+            if (!_cch) return;
+            _doClearAction(_key);
+            var _conf = _cch.conf;
+            if (_conf.retry>0){
+                _conf.retry--;
+                _cch.timer = window.setTimeout(
+                    _doPlayAction._$bind(null,_key),
+                    _conf.interval||500
                 );
             }else{
-                _doErrorCallback(_event.code);
+                _doErrorCallback(_key,_event.code);
             }
         };
         /**
          * 停止单例音频播放
          * @api    {nej.e._$stopBgSound}
          * @see    {nej.e._$playBgSound}
+         * @param  {String} 播放标识
          * @return {Void}
          */
-        _e._$stopBgSound = function(){
-            if (!!_audio){
-                _audio._$stop();
+        _e._$stopBgSound = function(_key){
+            var _cch = _pcache[_key||'auto-audio'];
+            if (!!_cch){
+                _cch.audio._$stop();
             }
         };
         return function(_url,_options){
-            _doStopAction();
-            _doStateChangeCallback(0);
-            _playing = NEJ.X({
-                url:_url
-            },_options);
-            _doPlayAction();
+            if (!_url) return;
+            var _playing = NEJ.X({},_options),
+                _key = _playing.key||'auto-audio',
+                _cch = _pcache[_key];
+            // stop last
+            if (!!_cch){
+                _doClearAction(_key);
+                _doStateChangeCallback(_key,0);
+            }
+            // save audio config
+            _pcache[_key] = {
+                url:_url,
+                conf:_playing,
+            };
+            // play current
+            _doPlayAction(_key);
         };
     })();
 };
