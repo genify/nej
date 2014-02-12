6bead7aa4c5b951fefd80e1bb86147a35423e3f9
unknown <genif_000@.(none)>
2014-02-12 16:40:41 +0800

    json string safe check

--- src/patched/trident-0/json.js
+++ src/patched/trident-0/json.js
@@ -11,14 +11,22 @@ var f = function(){
         _p = _('nej.p');
     if (_p._$NOT_PATCH.trident0) return;
     // eval for big string
-    JSON.parse = 
-    JSON.parse._$aop(function(_event){
-        var _str = _event.args[0]||'';
-        if (_str.length>=500000){
-            _event.stopped = !0;
-            _event.value = eval('('+_str+')');
-        }
-    });
+    JSON.parse = (function(){
+        // check save json string
+        // http://www.ietf.org/rfc/rfc4627.txt
+        var _isSafeJSON = function(_content){
+            return !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
+                        _content.replace(/"(\\.|[^"\\])*"/g,'')
+                    ));
+        };
+        return JSON.parse._$aop(function(_event){
+            var _str = _event.args[0]||'';
+            if (_str.length>=500000){  // &&_isSafeJSON(_str)
+                _event.stopped = !0;
+                _event.value = eval('('+_str+')');
+            }
+        });
+    })();
 };
 NEJ.define('{lib}patched/trident-0/json.js',
           ['{lib}util/encode/json.js'
