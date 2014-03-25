758ecd1e043e050c9deb012396d567114e0e311b
genify <genify@163.com>
2014-03-25 20:59:07 +0800

    support dependency config for define.js

---
+++ demo/api/define/deps.js
@@ -0,0 +1,6 @@
+NEJ.config({
+    '{pro}a.js':['{pro}b.js','{pro}c.js','{pro}d.js'],
+    '{pro}b.js':['{pro}c.js','{pro}e.js'],
+    '{pro}c.js':['{pro}d.js','{pro}e.js','{pro}a.js'],
+    '{pro}d.js':['{pro}e.js']
+});
