0f46a58711685fab6e2254d30e70c072f863cace
sean <java0race@163.com>
2014-05-27 15:56:26 +0800

    mp3 ui widget

---
+++ qunit/javascript/ui/mp3Test.js
@@ -0,0 +1,40 @@
+var f = function(){
+    //定义测试模块
+    module("ui-mp3");
+    var _  = NEJ.P,
+        _e = _('nej.e'),
+        _p = _('nej.ui');
+        
+    //开始单元测试
+    test('mp3',function(){
+    	expect(0);
+        var _mp3 = _p._$$MP3Player._$allocate({
+            parent:'box',
+            mode:0,
+            list:['../../../qunit/res/毕业纪念册 - 豆瓣FM.mp3',
+                  '../../../qunit/res/海阔天空 - 豆瓣FM.mp3',
+                  '../../../qunit/res/开始懂了 - 豆瓣FM.mp3']
+        });
+        // // 5秒后切换歌曲列表
+        // setTimeout(function(){
+        //     _mp3._$refreshList(['../../../qunit/res/海阔天空 - 豆瓣FM.mp3',
+        //                    '../../../qunit/res/开始懂了 - 豆瓣FM.mp3'],1);
+        // }._$bind(this),5000);
+
+        // setTimeout(function(){
+        //     _p._$$MP3Player._$recycle(_mp3);
+        // }._$bind(this),10000);
+
+        // setTimeout(function(){
+        //     var _mp3 = _p._$$MP3Player._$allocate({
+        //     parent:'box',
+        //     mode:0,
+        //     list:['../../../qunit/res/开始懂了 - 豆瓣FM.mp3']
+        // });
+        // }._$bind(this),15000);
+    });
+}
+module('依赖模块');
+test('define',function(){expect(0);
+    define('{pro}ui/mp3Test.js',['{lib}ui/audio/mp3.js','{pro}log.js'],f);
+});
---
+++ src/ui/audio/mp3.js
@@ -0,0 +1,411 @@
+var f = function() {
+	var _ = NEJ.P,
+		f = NEJ.F,
+		_e = _('nej.e'),
+		_v = _('nej.v'),
+		_c = _('nej.c'),
+		_p = _('nej.ui'),
+		_ut= _('nej.ut'),
+		__proMP3Player,
+        __supMP3Player;
+    var _seed_css  = _e._$pushCSSText('\
+                    .#<uispace> .m-pre, .#<uispace> .m-play, .#<uispace> .m-next, .#<uispace> .m-cur, .#<uispace> .m-pause, .#<uispace> .m-volmin,\
+					.#<uispace> .m-volminc, .#<uispace> .m-shuffled, .#<uispace> .m-repeatd-1, .#<uispace> .m-shufflec,\
+					.#<uispace> .m-repeatd, .#<uispace> .m-repeatc,\
+					.#<uispace> .m-volmax, .#<uispace> .m-volmaxc{background:url('+_c._$get('root')+'audio_sprite.png) no-repeat 9999px 9999px;}\
+					.#<uispace> .m-player{height:40px;min-width:530px;background:-webkit-gradient(linear,0% 0%, 0% 100%, from(#606060), to(#4e4e4e));background:-moz-linear-gradient(top, #606060, #4e4e4e);cursor:default}\
+					.#<uispace> .m-player .ctl{width:285px; float:left;}\
+					.#<uispace> .m-pre{height:30px;width:25px;background-position:0 0;float:left;margin-top:8px;margin-left: 10px;}\
+					.#<uispace> .m-pre:active, .#<uispace> .m-preatv{background-position:-104px -1px;}\
+					.#<uispace> .m-play{height:40px;width:32px;background-position:0 -35px;float:left;margin:2px 4px 0;}\
+					.#<uispace> .m-play:active{background-position:-202px -36px;}\
+					.#<uispace> .m-pause{height:40px;width:32px;background-position:-103px -35px;float:left;margin:2px 4px 0;}\
+					.#<uispace> .m-pause:active{background-position:-155px -36px;}\
+					.#<uispace> .m-next{height:30px;width:25px;background-position:0 -80px;float:left;margin-top:8px;}\
+					.#<uispace> .m-next:active, .#<uispace>  .m-nextatv{background-position:-103px -81px;}\
+					.#<uispace> .m-player .loop{width:120px;float:right}\
+					.#<uispace> .m-curtime{float: right;margin-right: 10px;color:#fff;line-height: 40px;}\
+					.#<uispace> .m-time{float: left;margin-left: 10px;color:#fff;line-height: 40px;}\
+					.#<uispace> .m-player .timeline{height:40px;position:absolute;left:285px;right:120px;bottom:0;top:0;min-width:120px}\
+					.#<uispace> .m-vol{height:27px;width:90px;padding:13px 0 0 20px;float:left;}\
+					.#<uispace> .m-volicn{width:12px;height:20px;margin:10px 0 0 4px;float:left}\
+					.#<uispace> .m-volmin{background-position:1px -185px;width:5px;height:20px;}\
+					.#<uispace> .m-volminc{background-position:-40px -186px;width:5px;height:20px;}\
+					.#<uispace> .m-volmax{background-position:-7px -185px;width:7px;margin-left:-1px;height:20px;}\
+					.#<uispace> .m-volmaxc{background-position:-50px -186px;width:7px;margin-left:-1px;height:20px;}\
+					.#<uispace> .m-timeline{position: relative;height:8px;border-radius:4px;margin-top:15px;background:-webkit-gradient(linear,0% 0%, 0% 100%, from(#2b2b2b), to(#3b3b3b));border-top:1px solid #212121;border-bottom:1px solid #636363}\
+					.#<uispace> .m-timeline-1{margin-top:0px;width:90px;position:absolute;}\
+					.#<uispace> .m-cur{background-position:-73px -186px;height:10px;width:10px;\
+						position:absolute;top:-1px}\
+					.#<uispace> .m-cur2{position: absolute;left: 80px;background:url('+_c._$get('root')+'audio_sprite.png) no-repeat 9999px 9999px;top: -1px;width:10px;background-position: -73px -186px;}\
+					.#<uispace> .m-cur:active{background-position:-93px -186px;}\
+					.#<uispace> .m-shuffleb{height:20px;width:20px;float:right;margin:10px 10px 0 0 }\
+					.#<uispace> .m-shuffled{background-position:-55px -115px;}\
+					.#<uispace> .m-shufflec{background-position:-56px -115px;}\
+					.#<uispace> .m-repeatb, .m-repeatb-1{height:20px;width:20px;float:right;margin:10px 10px 0 0 }\
+					.#<uispace> .m-repeatd{background-position:-55px -149px;}\
+					.#<uispace> .m-repeatd-1{background-position:-11px -149px;}\
+					.#<uispace> .m-repeatc{background-position:-56px -149px;}\
+					.m-cnt{width:600px;position: relative;}\
+                     ');
+    var _seed_html = _e._$addNodeTemplate('\
+                     <div class="m-cnt '+_seed_css+'">\
+						<div class="cse">\
+							<div class="m-player">\
+								<div class="ctl">\
+									<span class="f-ib m-pre ztag">&nbsp;</span> <span class="f-ib ztag m-play">&nbsp;</span> <span class="f-ib m-next ztag" value="next">&nbsp;</span><span class="m-vol">\
+										<span class="f-ib m-timeline m-timeline-1 ztag"> <span class="f-ib m-cur2 ztag">&nbsp;</span></span></span><span class="m-volicn f-ib "><span class="f-ib ztag m-volminc" style="opacity: 1;">&nbsp;</span><span class="f-ib ztag m-volmaxc" style="opacity: 100;">&nbsp;</span>\
+									</span><span class="m-curtime ztag">00:01</span>\
+								</div>\
+								<div class="timeline">\
+									<div class="m-timeline ztag">\
+										<span class="m-cur ztag" style="left: 1px;">&nbsp;</span>\
+									</div>\
+								</div>\
+								<div class="loop">\
+									<span class="m-time ztag">4:36</span><span class="m-repeatb m-repeatd ztag">&nbsp;</span>\
+								</div>\
+							</div>\
+						</div>\
+					</div>');
+    if (!!_p._$$MP3Player) return;
+    /**
+     * 音频播放器
+     * @type {[type]}
+     */
+    _p._$$MP3Player = NEJ.C();
+    __proMP3Player = _p._$$MP3Player._$extend(_p._$$Abstract);
+    __supMP3Player = _p._$$MP3Player._$supro;
+
+    /**
+     * 初始化播放器结构
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__initXGui = function(){
+    	this.__seed_css = _seed_css;
+    	this.__seed_html = _seed_html;
+    };
+
+    /**
+     * [__init description]
+     * @param  {[type]} _options [description]
+     * @return {[type]}          [description]
+     */
+    __proMP3Player.__init = function(_options){
+    	this.__supInit(_options);
+    	this.__isAutoStopped = true;
+    };
+
+    /**
+     * [__initNode description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__initNode = function(){
+    	this.__supInitNode();
+    	this.__ztags = _e._$getByClassName(this.__body,'ztag');
+    	this.__doInitDomEvent([
+    		[this.__ztags[0],'click',this.__onPre._$bind(this)],
+    		[this.__ztags[1],'click',this.__onPlay._$bind(this)],
+    		[this.__ztags[2],'click',this.__onNext._$bind(this)],
+    		[this.__ztags[11],'click',this.__onChangeState._$bind(this)]
+    	]);
+    };
+
+    /**
+     * [__reset description]
+     * @param  {[type]} _options [description]
+     * @return {[type]}          [description]
+     */
+    __proMP3Player.__reset = function(_options){
+    	this.__supReset(_options);
+	    if(!this.__audio)
+	        this.__audio =  _e._$audio({
+		        preload:_options.preload||false,
+		        onstatechange:this.__onStateChange._$bind(this),
+		        ontimeupdate:this.__onTimeUpdate._$bind(this),
+		        onvolumechange:this.__onVolumeChange._$bind(this)
+		    });
+    	if(!this.__timeSlider)
+	    	this.__timeSlider = _ut._$$SliderX._$allocate({
+	            slide:this.__ztags[9],
+	            track:this.__ztags[8],
+	            onchange:this.__onTimeSlideChange._$bind(this)
+	        });
+	    if(!this.__volSlider)
+		    this.__volSlider = _ut._$$SliderX._$allocate({
+	            slide:this.__ztags[4],
+	            track:this.__ztags[3],
+	            onchange:this.__onVolSlideChange._$bind(this)
+	        });
+		this._$refreshList(_options.list,_options.mode);
+    };
+
+    /**
+     * [__destroy description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__destroy = function(){
+    	this.__supDestroy();
+    	if(!!this.__audio) this.__audio = this.__audio._$recycle();
+    	if(!!this.__timeSlider) this.__timeSlider = this.__timeSlider._$recycle();
+    	if(!!this.__volSlider) this.__volSlider = this.__volSlider._$recycle();
+    };
+
+    /**
+     * [__onVolumeChange description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onVolumeChange = f;
+
+    /**
+     * [__onChangeState description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onChangeState = function(_event){
+    	var _node = _v._$getElement(_event);
+    	if(_e._$hasClassName(_node,'m-repeatd')){
+    		_e._$replaceClassName(_node,'m-repeatb m-repeatd','m-shuffleb m-shuffled');
+    		this.__playlist._$setPlayMode(2);
+    	}else if(_e._$hasClassName(_node,'m-shuffled')){
+    		_e._$replaceClassName(_node,'m-shuffleb m-shuffled','m-repeatb-1 m-repeatd-1');
+    		this.__playlist._$setPlayMode(1);
+    	}else if(_e._$hasClassName(_node,'m-repeatd-1')){
+    		_e._$replaceClassName(_node,'m-repeatb-1 m-repeatd-1','m-repeatb m-repeatd');
+    		this.__playlist._$setPlayMode(0);
+    	}
+    };
+
+    /**
+     * [__onPre description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onPre = function(){
+    	this.__isAutoStopped = false;
+    	this.__playlist._$next();
+    };
+
+    /**
+     * [__onPlay description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onPlay = function(_event){
+		if(this.__state == 3){
+			this.__audio._$play();
+		}else if(this.__state == 2){
+			this.__audio._$pause();
+		}else if(this.__state == 0){
+			this.__musicPlay();
+		}
+    };
+
+    /**
+     * [__onNext description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onNext = function(){
+    	this.__isAutoStopped = false;
+    	this.__playlist._$prev();
+    };
+
+    /**
+     * [__loadMusic description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__loadMusic = function(_url){
+    	this.__audio._$source(_url);
+	    setTimeout(function(){
+	    	this.__audio._$play();
+	    }._$bind(this),0);
+    };
+
+    /**
+     * [__updateStateStyle description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__updateStateStyle = function(){
+    	if(this.__state == 0){
+    		_e._$replaceClassName(this.__ztags[1],'m-pause','m-play');
+			this.__currentTime = null;
+			this.__duration = null;
+			this.__ztags[7].innerHTML = '00:00';
+			this.__ztags[10].innerHTML = '00:00';
+			_e._$setStyle(this.__ztags[9],'left','0px');
+    	}else if(this.__state == 2){
+    		_e._$replaceClassName(this.__ztags[1],'m-play','m-pause');
+    	}else if(this.__state == 3){
+    		_e._$replaceClassName(this.__ztags[1],'m-pause','m-play');
+    	}
+    };
+
+    /**
+     * [__onStateChange description]
+     * @param  {[type]} _event [description]
+     * @return {[type]}        [description]
+     */
+    __proMP3Player.__onStateChange = function(_event){
+    	this.__state = _event.state;
+    	this.__updateStateStyle();
+		if(this.__state == 0){
+			// 当前停止状态
+			this.__audio._$seek(0);
+			// 是否歌曲自己播放完了,如果是，自动播放下一首
+			if(this.__isAutoStopped){
+				if(this.__playlist._$getPlayMode() == 1){
+					// 单曲循环的情况
+					var _index = this.__playlist._$getPlayIndex();
+					this.__playlist._$setPlayIndex(_index);
+				}else{
+					this.__playlist._$next();
+				}
+			}
+		}else if(this.__state == 2){
+    		// 当前播放状态
+			// 播放的时候，设置歌曲自己播放完
+			this.__isAutoStopped = true;
+		}
+    };
+
+    /**
+     * [__onTimeUpdate description]
+     * @param  {[type]} _event [description]
+     * @return {[type]}        [description]
+     */
+    __proMP3Player.__onTimeUpdate = function(_event){
+    	// 播放第二首的current有问题
+	    if(parseInt(_event.current) === 0){
+	    	this.__onAudioLoaded(_event);
+	    }
+	    this.__onAudioTimeupdate(_event);
+    };
+
+    /**
+     * [__onAudioLoaded description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__onAudioLoaded = function(_event){
+		var _time = _event.duration;
+		this.__ztags[7].innerHTML ='00:00';
+		this.__ztags[10].innerHTML =  this.__formatSecond(_time);
+		var _left = Math.floor(this.__ztags[3].clientWidth - 10) + 'px';
+		_e._$setStyle(this.__ztags[4],'left',_left);
+		this.__refreshVolume(this.__audio._$volume());
+    };
+
+    /**
+     * [__formatSecond description]
+     * @return {[type]} [description]
+     */
+    __proMP3Player.__formatSecond = function(_second){
+    	if(_second>=3600){
+    		var _hour = parseInt(_second/3600),
+    			_min  = parseInt((_second%3600)/60),
+    			_sec  = parseInt(_second%60);
+    		_min = _min > 9 ? _min : '0' + _min;
+    		_sec = _sec > 9 ? _sec : '0' + _sec;
+    		return _hour + ':' + _min + ':' +  _sec;
+    	}else{
+    		var _min  = parseInt(_second/60),
+    			_sec  = parseInt(_second%60);
+    		_min = _min > 9 ? _min : '0' + _min;
+    		_sec = _sec > 9 ? _sec : '0' + _sec;
+    		return _min + ':' +  _sec;
+    	}
+    };
+
+    /**
+     * [__onAudioTimeupdate description]
+     * @param  {[type]} _event [description]
+     * @return {[type]}        [description]
+     */
+    __proMP3Player.__onAudioTimeupdate = function(_event){
+        if(!!this.__timelineMouseDown) return;
+        var _time = _event.current;
+        this.__currentTime = _time;
+        this.__duration = _event.duration;
+        this.__ztags[7].innerHTML = this.__formatSecond(_time);
+        var _played = this.__currentTime / this.__duration;
+        var _left = Math.floor((this.__ztags[8].clientWidth - 10) * _played) + 'px';
+        _e._$setStyle(this.__ztags[9],'left',_left);
+    };
+
+    /**
+     * [__onTimeSlideChange description]
+     * @param  {[type]} _event [description]
+     * @return {[type]}        [description]
+     */
+    __proMP3Player.__onTimeSlideChange = function(_event){
+    	if(!this.__duration || !this.__currentTime) return;
+    	var _value = _event.x.value;
+    	_e._$setStyle(this.__ztags[9],'left',_value + 'px');
+    	this.__timelineMouseDown = true;
+    	if(!!_event.stopped){
+    		var _time = (_value / (this.__ztags[8].clientWidth - 10)) * this.__duration;
+    		this.__currentTime = _time;
+        	this.__audio._$seek(this.__currentTime);
+    		this.__timelineMouseDown = false;
+    	}
+    };
+
+    /**
+     * [__onVolSlideChange description]
+     * @param  {[type]} _event [description]
+     * @return {[type]}        [description]
+     */
+    __proMP3Player.__onVolSlideChange = function(_event){
+    	var _value = _event.x.value;
+    	_e._$setStyle(this.__ztags[4],'left',_value + 'px');
+    	var _volume = (_value - 10)/(this.__ztags[3].clientWidth);
+	    this.__refreshVolume(_volume);
+    };
+
+    /**
+     * [__refreshVolume description]
+     * @param  {[type]} _volume [description]
+     * @return {[type]}         [description]
+     */
+    __proMP3Player.__refreshVolume = function(_volume){
+		this.__audio._$volume(_volume * 100);
+		if(_volume < 0.5){
+			_e._$replaceClassName(this.__ztags[5],'m-volmin','m-volminc');
+			_e._$replaceClassName(this.__ztags[6],'m-volmaxc','m-volmax');
+			this.__ztags[5].style.opacity = 1;
+			if(_volume / 0.5 < 0.3){
+			  _e._$replaceClassName(this.__ztags[5],'m-volminc','m-volmin');
+			  this.__ztags[5].style.opacity = 1;
+			}else
+			  this.__ztags[5].style.opacity = _volume / 0.5;
+		}else{
+			_e._$replaceClassName(this.__ztags[5],'m-volmin','m-volminc');
+			_e._$replaceClassName(this.__ztags[6],'m-volmax','m-volmaxc');
+			_e._$setStyle(this.__ztags[5],'opacity',1);
+			_e._$setStyle(this.__ztags[6],'opacity',_volume);
+		}
+    };
+
+    /**
+     * [_$refreshList description]
+     * @param  {[type]} _list [description]
+     * @return {[type]}       [description]
+     */
+    __proMP3Player._$refreshList = function(_list,_mode){
+    	if(!!this.__playlist)
+    		_ut._$$PlayList._$recycle(this.__playlist)
+		this.__playlist = _ut._$$PlayList._$allocate({
+			mode:_mode||0,
+			list:_list||[],
+			onmodechange:function(_event){
+			},
+			onmediachange:function(_event){
+				var _url = _event.list[_event.index];
+				this.__loadMusic(_url);
+				}._$bind(this)
+			});
+    };
+};
+NEJ.define('{lib}ui/audio/mp3.js',
+      ['{lib}ui/base.js'
+      ,'{patch}audio.js'
+      ,'{patch}config.js'
+      ,'{lib}util/media/playlist.js'
+      ,'{lib}util/slider/slider.x.js'
+      ,'{lib}util/media/audio.js'],f);
