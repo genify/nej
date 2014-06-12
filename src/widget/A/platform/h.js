9940c44b835019cce96cebfc4a84c38e7d64378c
sean <java0race@163.com>
2014-06-12 19:20:23 +0800

    remove widget test

--- src/widget/A/platform/h.js
+++
@@ -1,4 +0,0 @@
-var f = function() {
-	console.log('exec h.js');
-};
-define(['{lib}base/event.js'],f);
--- src/widget/A/src/a.js
+++
@@ -1,4 +0,0 @@
-var f = function() {
-	console.log('my widget file');
-};
-define('{lib}widget/A/src/a.js',['../{platform}h.js','{lib}base/element.js'],f);
