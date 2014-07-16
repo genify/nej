1c2726dcf13d9fc6eb620dd93db466cbaade6e10
sean <java0race@163.com>
2014-07-16 19:25:56 +0800

    editor linkcard

--- qunit/javascript/ui/links.js
+++ qunit/javascript/ui/links.js
@@ -21,44 +21,15 @@ function(){
      * @param   {Object} 可选配置参数，已处理参数列表如下
      *                           
      */
-    _p._$$Link2  = NEJ.C();
-    var proto = _p._$$Link2._$extend(nej.ut.cmd._$$Link);
+    _p._$$MyLink  = NEJ.C();
+    var proto = _p._$$MyLink._$extend(nej.ut.cmd._$$Link);
 
     /*
      * 命令名称
      * @type {String}
      */
-    _p._$$Link2.command = 'link';
-    /**
-     * 链接错误提示
-     * @param  {Object} type 错误类型
-     * @return {Void}
-     */
-    proto.__onError = function(){
-        this.__linkCard.__showErrorTips('请输入合法的链接地址（http://或https://）');
-    };
-    // /**
-    //  * 显示卡片，一般子类重写
-    //  * @protected
-    //  * @method {__doShowCard}
-    //  * @return {Void}
-    //  */
-    // proto.__doShowCard = function(){
-    //     this.__fopt.name = this.__editor._$getSelectText();
-    //     this.__linkCard = _i._$$LinkCard._$allocate({
-    //         draggable: true,
-    //         destroyable: true,
-    //         maskclazz: 'm-mask',
-    //         name: this.__fopt.name,
-    //         clazz: 'm-layer m-layer-link',
-    //         title: '添加超链接',
-    //         onchange: this.__onChange._$bind(this),
-    //         onErrorLink: this.__onError._$bind(this)
-    //     });
-    //     this.__linkCard._$show();
-    //     this.__linkCard._$doFocus();
-    // };
+    _p._$$MyLink.command = 'link';
     
     // regist command implemention
-    _p._$$Link2._$regist();
+    _p._$$MyLink._$regist();
 });
