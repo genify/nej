d621c6421ad88e8461929e5a638bfffbd171b0f1
genify <genify@163.com>
2014-08-01 18:10:36 +0800

    refactor util/event/event.js

--- src/util/event/event.js
+++ src/util/event/event.js
@@ -5,35 +5,44 @@
  * @author   genify(caijf@corp.netease.com)
  * ------------------------------------------
  */
-var f = function(){
-    // variable declaration
-    var _  = NEJ.P,
-        _o = NEJ.O,
-        _f = NEJ.F,
-        _e = _('nej.e'),
-        _v = _('nej.v'),
-        _u = _('nej.u'),
-        _p = _('nej.ut'),
-        _pro;
-    if (!!_p._$$CustomEvent) return;
+NEJ.define([
+    '{lib}base/global.js',
+    '{lib}base/klass.js',
+    '{lib}base/element.js',
+    '{lib}base/event.js',
+    '{lib}base/util.js',
+    '{lib}util/event.js'
+],function(NEJ,_k,_e,_v,_u,_t,_p,_o,_f,_r){
     /**
-     * 自定义事件封装对象，封装的事件支持通过事件相关接口进行添加、删除等操作
+     * 自定义事件封装对象，封装的事件支持通过事件相关接口进行添加、删除等操作<br/>
      * 
      * 脚本举例
      * [code]
-     *   // 添加自定义事件 
-     *   nej._v._$addEvent(window,'ok',function(){alert(0);});
-     *   nej._v._$addEvent(window,'ok',function(){alert(1);});
-     *   // 删除自定义事件
-     *   nej._v._$delEvent(window,'ok',function(){alert(0);});
-     *   nej._v._$clearEvent(window,'ok');
-     *   // 触发自定义事件
-     *   window.onok({a:'aaaaa'});
-     *   nej._v._$dispatchEvent(window,'ok',{a:'aaaaa'});
+     *   NEJ.define([
+     *       '{lib}base/event.js'
+     *       '{lib}util/event.js'
+     *   ],function(_v,_t){
+     *       // 支持自定义事件
+     *       _t._$$CustomEvent._$allocate({
+     *           element:window,
+     *           event:'onok'
+     *       });
+     * 
+     *       // 添加自定义事件 
+     *       _v._$addEvent(window,'ok',function(){alert(0);});
+     *       _v._$addEvent(window,'ok',function(){alert(1);});
+     *       // 删除自定义事件
+     *       _v._$delEvent(window,'ok',function(){alert(0);});
+     *       _v._$clearEvent(window,'ok');
+     *       // 触发自定义事件
+     *       window.onok({a:'aaaaa'});
+     *       _v._$dispatchEvent(window,'ok',{a:'aaaaa'});
+     *   });
      * [/code]
      * 
-     * @class   {nej.ut._$$CustomEvent} 自定义事件封装对象
-     * @extends {nej.ut._$$Event}
+     * @class   {_$$CustomEvent}
+     * @extends {_$$Event}
+     * 
      * @param   {Object} 可选配置参数，已处理参数列表如下
      * @config  {String|Node}  element 事件关联节点ID或者对象，默认为window对象
      * @config  {String|Array} event   事件名称或者名称列表
@@ -53,8 +62,8 @@ var f = function(){
      * @event  {oneventadd}
      * @param  {Object} 事件信息
      */
-    _p._$$CustomEvent = NEJ.C();
-    _pro = _p._$$CustomEvent._$extend(_p._$$Event);
+    _p._$$CustomEvent = _k._$klass();
+    _pro = _p._$$CustomEvent._$extend(_t._$$Event);
     /**
      * 控件初始化
      * @protected
@@ -65,7 +74,7 @@ var f = function(){
         // onxxx - event entry handler
         //   xxx - event callback handler list
         this.__cache = {};
-        this.__supInit();
+        this.__super();
     };
     /**
      * 控件重置
@@ -75,7 +84,7 @@ var f = function(){
      * @return {Void}
      */
     _pro.__reset = function(_options){
-        this.__supReset(_options);
+        this.__super(_options);
         this.__element = _e._$get(_options.element)||window;
         // init event
         this.__doEventInit(_options.event);
@@ -96,7 +105,7 @@ var f = function(){
             delete _map[_key];
         };
         return function(){
-            this.__supDestroy();
+            this.__super();
             // clear cache
             _u._$forIn(
                 this.__cache,_doClear,this
@@ -115,7 +124,7 @@ var f = function(){
     _pro.__isDelegate = function(_element,_type){
         _element = _e._$get(_element);
         return _element===this.__element&&
-             (!_type||!!this.__cache['on'+_type]);
+               (!_type||!!this.__cache['on'+_type]);
     };
     /**
      * 初始化事件
@@ -132,7 +141,8 @@ var f = function(){
                     this.__doEventDispatch.
                         _$bind(this,_event);
             }
-            this.__doEventBind(_event); return;
+            this.__doEventBind(_event); 
+            return;
         }
         if (_u._$isArray(_event)){
             _u._$forEach(
@@ -153,8 +163,9 @@ var f = function(){
             _handler1 = this.__cache[_event];
         if (_handler!=_handler1){
             this.__doEventDelete(_type);
-            if (!!_handler&&_handler!=_f)
+            if (!!_handler&&_handler!=_f){
                 this.__doEventAdd(_type,_handler);
+            }
             this.__element[_event] = _handler1;
         }
     };
@@ -213,7 +224,9 @@ var f = function(){
      */
     _pro.__doEventDispatch = function(_type,_event){
         _event = _event||{noargs:!0};
-        if (_event==_o) _event = {};
+        if (_event==_o){
+            _event = {};
+        }
         _event.type = _type;
         this._$dispatchEvent('ondispatch',_event);
         if (!!_event.stopped) return;
@@ -284,8 +297,10 @@ var f = function(){
         };
         return function(){
             // void multi-enhance
-            if (!!this.__enhanced)
+            if (!!this.__enhanced){
                 return;
+            }
+            // enhance event api
             this.__enhanced = true;
             _v._$addEvent = 
             _v._$addEvent._$aop(_doAddEvent._$bind(this));
@@ -297,12 +312,10 @@ var f = function(){
             _v._$dispatchEvent._$aop(_doDispatchEvent._$bind(this));
         };
     })();
-};
-// define dependency
-NEJ.define(
-    '{lib}util/event/event.js',[
-    '{lib}base/util.js',
-    '{lib}base/event.js',
-    '{lib}base/element.js',
-    '{lib}util/event.js'
-],f);
+    
+    if (CMPT){
+        NEJ.copy(NEJ.P('nej.ut'),_p);
+    }
+    
+    return _p;
+});
