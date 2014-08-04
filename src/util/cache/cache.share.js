b05e87eabee54d67247dff3039e78c30528f33d3
genify <genify@163.com>
2014-08-04 14:34:00 +0800

    refactor cache.share.js

--- src/util/cache/cache.share.js
+++ src/util/cache/cache.share.js
@@ -5,43 +5,47 @@
  * @author   genify(caijf@corp.netease.com)
  * ------------------------------------------
  */
-var f = function(){
-    // variable declaration
-    var _  = NEJ.P,
-        _v = _('nej.v'),
-        _u = _('nej.u'),
-        _p = _('nej.ut'),
-        _proShareCache;
-    if (!!_p._$$ShareCache) return;
+NEJ.define([
+    '{lib}base/global.js',
+    '{lib}base/klass.js',
+    '{lib}base/util.js',
+    '{lib}util/event.js',
+    '{lib}util/event/event.js'
+],function(NEJ,_k,_u,_t0,_t1,_p,_o,_f,_r){
+    var _pro;
     /**
      * 临时内存数据共享管理器<br/>
      * 
      * 脚本示例：
      * [code]
-     *   // 统一定义名字空间简写
-     *   var _  = NEJ.P,
-     *       _v = _('nej.v');
+     *   NEJ.define([
+     *       '{lib}base/event.js',
+     *       '{lib}util/cache/cache.share.js'
+     *   ],function(_v,_p){
+     *       // 添加缓存变化监测事件
+     *       _v._$addEvent(
+     *           localCache,'cachechange',
+     *           function(_event){
+     *               // _event.key
+     *               // _event.type
+     *               // _event.oldValue
+     *               // _event.newValue
+     *           }
+     *       );
      * 
-     *   // 添加缓存变化监测事件
-     *   _v._$addEvent(
-     *          localCache,'cachechange',
-     *       function(_event){
-     *              // _event.key
-     *           // _event.type
-     *           // _event.oldValue
-     *           // _event.newValue
-     *       }
-     *   );
-     *   // 写入数据
-     *   localCache._$set('abc',{a:'aaaaa',b:'bbbbbb'});
-     *   // 读取数据
-     *   var _data = localCache._$get('abc');
-     *   // 删除数据
-     *   localCache._$remove('abc');
+     *       // 写入数据
+     *       localCache._$set('abc',{a:'aaaaa',b:'bbbbbb'});
+     * 
+     *       // 读取数据
+     *       var _data = localCache._$get('abc');
+     * 
+     *       // 删除数据
+     *       localCache._$remove('abc');
+     *   });
      * [/code]
      * 
-     * @class   {nej.ut._$$ShareCache}
-     * @extends {nej.ut._$$Event}
+     * @class   {_$$ShareCache}
+     * @extends {_$$Event}
      * 
      * [hr]
      * 缓存变化出发事件
@@ -52,19 +56,19 @@ var f = function(){
      * @config {Variable} oldValue 原数据
      * @config {Variable} newValue 新数据
      */
-    _p._$$ShareCache = NEJ.C();
-      _proShareCache = _p._$$ShareCache._$extend(_p._$$Event);
+    _p._$$ShareCache = _k._$klass();
+    _pro = _p._$$ShareCache._$extend(_t0._$$Event);
     /**
      * 初始化函数
      * @protected
      * @method {__init}
      * @return {Void}
      */
-    _proShareCache.__init = (function(){
+    _pro.__init = (function(){
         var _seed = +new Date,
             _ckey = 'dat-'+_seed;
         return function(){
-            this.__supInit();
+            this.__super();
             var _cache = this.constructor[_ckey];
             if (!_cache){
                 _cache = {};
@@ -79,7 +83,7 @@ var f = function(){
       * @param  {Object}   缓存标识
       * @return {Variable} 缓存数据
      */
-    _proShareCache._$get = function(_key){
+    _pro._$get = function(_key){
         return this.__cache[_key];
     };
     /**
@@ -89,7 +93,7 @@ var f = function(){
       * @param  {Variable} 缓存数据
       * @return {Void}
      */
-    _proShareCache._$set = function(_key,_value){
+    _pro._$set = function(_key,_value){
         var _old = this.__cache[_key];
         this.__cache[_key] = _value;
         _v._$dispatchEvent(
@@ -98,15 +102,15 @@ var f = function(){
                 type:'set',
                 oldValue:_old,
                 newValue:_value
-            });
-        return this;
+            }
+        );
     };
     /**
      * 删除缓存项
      * @param  {String}   缓存标识
      * @return {Variable} 删除项的值
      */
-    _proShareCache._$remove = function(_key){
+    _pro._$remove = function(_key){
         var _old = this.__cache[_key];
         _u._$safeDelete(this.__cache,_key);
         _v._$dispatchEvent(
@@ -115,7 +119,8 @@ var f = function(){
                 type:'delete',
                 oldValue:_old,
                 newValue:undefined
-            });
+            }
+        );
         return _old;
     };
     /**
@@ -123,16 +128,23 @@ var f = function(){
      * @param  {String}   查询串，如：a.b.c
      * @return {Variable} 结果
      */
-    _proShareCache._$query = function(_namespace){
-        return NEJ.Q(this.__cache,_namespace);
+    _pro._$query = function(_namespace){
+        return _u._$query(this.__cache,_namespace);
     };
+    
     // local cache instance
-    window.localCache = _p._$$ShareCache._$allocate();
+    if (!this.localCache){
+        this.localCache = _p._$$ShareCache._$allocate();
+    }
     // custom event
-    _p._$$CustomEvent._$allocate({
+    _t1._$$CustomEvent._$allocate({
         element:localCache,
         event:'cachechange'
     });
-};
-NEJ.define('{lib}util/cache/cache.share.js',
-          ['{lib}util/event/event.js'],f);
+    
+    if (CMPT){
+        NEJ.copy(NEJ.P('nej.ut'),_p);
+    }
+    
+    return _p;
+});
