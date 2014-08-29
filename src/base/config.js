8b28886ea024d2b7296f58324e2b8df74614d5ec
genify <genify@163.com>
2014-08-29 18:03:45 +0800

    bugfix for config.js

--- src/base/config.js
+++ src/base/config.js
@@ -8,24 +8,8 @@
 /** @module base/config */
 NEJ.define([
     './global.js',
-    '{platform}util.js',
     '{platform}config.js'
 ],function(NEJ,_u,_h,_p,_o,_f,_r){
-    /*
-     * URL地址转源信息
-     * http://a.b.com:8080/a/bc/ -> http://a.b.com:8080
-     * @param  {String} URL地址
-     * @return {String} 源信息
-     */
-    var _url2host = (function(){
-        var _reg = /^([\w]+?:\/\/.*?(?=\/|$))/i;
-        return function(_url){
-            _url = _url||'';
-            if (_reg.test(_url))
-                return RegExp.$1;
-            return location.protocol+'//'+location.host;
-        };
-    })();
     /**
      * 取Frame跨域Ajax代理文件，通过NEJ_CONF的p_frame配置给定域名的代理文件地址
      *
@@ -35,7 +19,7 @@ NEJ.define([
      * @return {String}        代理文件地址
      */
     _p._$getFrameProxy = function(_url){
-        var _host = _url2host(_url);
+        var _host = _h.__url2host(_url);
         return _p._$get('frames')[_host]||
               (_host+'/res/nej_proxy_frame.html');
     };
@@ -48,7 +32,7 @@ NEJ.define([
      * @return {String}        代理文件地址
      */
     _p._$getFlashProxy = function(_url){
-        return _p._$get('flashs')[_url2host(_url)];
+        return _p._$get('flashs')[_h.__url2host(_url)];
     };
     /**
      * 获取NEJ配置信息，通过NEJ_CONF配置相关信息
@@ -114,61 +98,6 @@ NEJ.define([
     _p._$get = function(_key){
         return _h.__get(_key);
     };
-    // init
-    /*
-     * 初始化配置信息
-     * @param  {Object} 配置信息
-     * @return {Void}
-     */
-    var _doInit = (function(){
-        var _conf = {
-            'portrait':{name:'portrait',dft:'portrait/'},
-            'ajax.swf':{name:'ajax',dft:'nej_proxy_flash.swf'},
-            'chart.swf':{name:'chart',dft:'nej_flex_chart.swf'},
-            'audio.swf':{name:'audio',dft:'nej_player_audio.swf'},
-            'video.swf':{name:'video',dft:'nej_player_video.swf'},
-            'clipboard.swf':{name:'clipboard',dft:'nej_clipboard.swf'},
-            'upload.image.swf':{name:'uploadimage',dft:'nej_upload_image.swf'}
-        };
-        var _doInitProxy = function(_list){
-            var _map = {};
-            if (!_list||!_list.length){
-                return _map;
-            }
-            for(var i=0,l=_list.length,_path;i<l;i++){
-                _path = _list[i];
-                if (_path.indexOf('://')>0)
-                    _map[_url2host(_path)] = _path;
-            }
-            return _map;
-        };
-        return function(_config){
-            // check path config
-            _h.__set('root',_config.root||'/res/');
-            var _root = _p._$get('root');
-            _u.__forIn(_conf,function(v,k,m){
-                _h.__set(k,_config[v.name]||(_root+v.dft));
-            });
-            // csrf config
-            var _csrf = _config.p_csrf;
-            if (_csrf===!0){
-                _csrf = {
-                    cookie:'AntiCSRF',
-                    param:'AntiCSRF'
-                };
-            }
-            _csrf = _csrf || _o;
-            _h.__set('csrf',{
-                param:_csrf.param||'',
-                cookie:_csrf.cookie||''
-            });
-            // ajax by frame proxy
-            _h.__set('frames',_doInitProxy(_config.p_frame));
-            // ajax by flash proxy
-            _h.__set('flashs',_doInitProxy(_config.p_flash));
-        };
-    })();
-    _doInit(this.NEJ_CONF||_o);
 
     if (CMPT){
         NEJ.copy(NEJ.P('nej.c'),_p);
--- src/base/platform/config.js
+++ src/base/platform/config.js
@@ -6,9 +6,25 @@
  * ------------------------------------------
  */
 NEJ.define([
-    'base/global'
-],function(NEJ,_p,_o,_f,_r){
+    'base/global',
+    'base/util',
+],function(NEJ,_u,_p,_o,_f,_r){
     var _cache = {};
+    /*
+     * URL地址转源信息
+     * http://a.b.com:8080/a/bc/ -> http://a.b.com:8080
+     * @param  {String} URL地址
+     * @return {String} 源信息
+     */
+    _p.__url2host = (function(){
+        var _reg = /^([\w]+?:\/\/.*?(?=\/|$))/i;
+        return function(_url){
+            _url = _url||'';
+            if (_reg.test(_url))
+                return RegExp.$1;
+            return location.protocol+'//'+location.host;
+        };
+    })();
     /**
      * 设置NEJ配置信息
      * @param  {String}   配置标识
@@ -26,6 +42,62 @@ NEJ.define([
     _p.__get = function(_key){
         return _cache[_key];
     };
+
+    // init
+    /*
+     * 初始化配置信息
+     * @param  {Object} 配置信息
+     * @return {Void}
+     */
+    var _doInit = (function(){
+        var _conf = {
+            'portrait':{name:'portrait',dft:'portrait/'},
+            'ajax.swf':{name:'ajax',dft:'nej_proxy_flash.swf'},
+            'chart.swf':{name:'chart',dft:'nej_flex_chart.swf'},
+            'audio.swf':{name:'audio',dft:'nej_player_audio.swf'},
+            'video.swf':{name:'video',dft:'nej_player_video.swf'},
+            'clipboard.swf':{name:'clipboard',dft:'nej_clipboard.swf'},
+            'upload.image.swf':{name:'uploadimage',dft:'nej_upload_image.swf'}
+        };
+        var _doInitProxy = function(_list){
+            var _map = {};
+            if (!_list||!_list.length){
+                return _map;
+            }
+            for(var i=0,l=_list.length,_path;i<l;i++){
+                _path = _list[i];
+                if (_path.indexOf('://')>0)
+                    _map[_p.__url2host(_path)] = _path;
+            }
+            return _map;
+        };
+        return function(_config){
+            // check path config
+            _p.__set('root',_config.root||'/res/');
+            var _root = _p._$get('root');
+            _u._$forIn(_conf,function(v,k,m){
+                _p.__set(k,_config[v.name]||(_root+v.dft));
+            });
+            // csrf config
+            var _csrf = _config.p_csrf;
+            if (_csrf===!0){
+                _csrf = {
+                    cookie:'AntiCSRF',
+                    param:'AntiCSRF'
+                };
+            }
+            _csrf = _csrf || _o;
+            _p.__set('csrf',{
+                param:_csrf.param||'',
+                cookie:_csrf.cookie||''
+            });
+            // ajax by frame proxy
+            _p.__set('frames',_doInitProxy(_config.p_frame));
+            // ajax by flash proxy
+            _p.__set('flashs',_doInitProxy(_config.p_flash));
+        };
+    })();
+    _doInit(this.NEJ_CONF||_o);
     
     return _p;
 });
