259139f6d1266e8e5d3d977ee18703f9cdaff191
java0racle <java0racle@163.com>
2014-09-02 21:16:40 +0800

    test

--- src/util/slider/test/slider.test.js
+++ src/util/slider/test/slider.test.js
@@ -1,12 +1,12 @@
 var f = function(){
     //定义测试模块
     module("slider");
-    
+
     var _  = NEJ.P,
         _e = _('nej.e'),
         _v = _('nej.v'),
         _p = _('nej.ut');
-    
+
     test('sliderX',function(){
         expect(0);
 		var _sd = _p._$$SliderX._$allocate({
@@ -18,7 +18,7 @@ var f = function(){
 			}
 		});
     });
-	
+
 	test('sliderY',function(){
         expect(0);
         var _sd = _p._$$SliderY._$allocate({
@@ -26,11 +26,11 @@ var f = function(){
             slide:'slide1',
             track:'track1',
             onchange:function(_obj){
-                _e._$style(_e._$get('slide'),{left:_obj.x.value});
+                // _e._$style(_e._$get('slide'),{'top':_obj.y.value});
             }
         });
     });
-	
+
 	test('sliderXY',function(){
         expect(0);
         var _sd = _p._$$SliderXY._$allocate({
@@ -50,4 +50,3 @@ test('define',function(){expect(0);
      '{lib}util/slider/slider.y.js',
      '{lib}util/slider/slider.xy.js'],f);
 });
-  
