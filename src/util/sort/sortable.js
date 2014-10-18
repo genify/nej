21884c4078742b0906304a4b2a8dc2acf4b85fa0
genify <genify@163.com>
2014-10-18 15:07:39 +0800

    support onthumbupdate/onholderupdate for sortable

--- src/util/sort/sortable.js
+++ src/util/sort/sortable.js
@@ -27,6 +27,7 @@ NEJ.define([
      * @property {String} clazz       - 可排序节点的类标识，默认为j-sortable
      * @property {String} trigger     - 触发排序节点的类标识，默认为clazz标识的节点
      * @property {Node}   placeholder - 占位符，用于标识插入位置
+     * @property {Node}   thumbnail   - 移动缩略图，跟随鼠标移动
      * @property {String} selected    - 排序节点选中样式，默认为j-selected
      */
     /** 
@@ -41,6 +42,33 @@ NEJ.define([
      * 
      * @event module:util/sort/sortable._$$Sortable#onsortchange
      * @param {Object} event - 事件信息
+     */
+    /** 
+     * 排序缩略图更新事件
+     * 
+     * @event    module:util/sort/sortable._$$Sortable#onthumbupdate
+     * @param    {Object}     event   - 事件信息
+     * @property {Varable}    state   - 状态值，1-开始排序，2-排序中，3-排序结束
+     * @property {Number}     top     - 鼠标距页面顶部位置
+     * @property {Number}     left    - 鼠标距页面左侧位置
+     * @property {Node}       target  - 缩略图节点
+     * @property {Node|Array} source  - 排序节点或者节点列表
+     * @property {Boolean}    stopped - 是否阻止后续逻辑
+     */
+    /** 
+     * 排序占位符更新事件
+     * 
+     * @event    module:util/sort/sortable._$$Sortable#onholderupdate
+     * @param    {Object}     event    - 事件信息
+     * @property {Number}     top      - 鼠标相对于容器顶部偏移量
+     * @property {Number}     left     - 鼠标相对于容器左侧偏移量
+     * @property {Number}     width    - 排序节点宽度
+     * @property {Number}     height   - 排序节点高度
+     * @property {String}     position - 占位符插入位置，beforeBegin/afterEnd
+     * @property {Node}       target   - 占位符节点
+     * @property {Node|Array} source   - 排序节点或者节点列表
+     * @property {Node}       ref      - 插入点参考节点
+     * @property {Boolean}    stopped  - 是否阻止后续逻辑
      */
     _p._$$Sortable = _k._$klass();
     _pro = _p._$$Sortable._$extend(_t._$$EventTarget);
@@ -60,6 +88,8 @@ NEJ.define([
         this.__viewport = _e._$get(_options.viewport);
         this.__parent = _e._$get(_options.parent);
         this.__holder = _e._$get(_options.placeholder);
+        this.__thumb  = _e._$get(_options.thumbnail);
+        _e._$removeByEC(this.__thumb);
         _e._$removeByEC(this.__holder);
         this.__doInitDomEvent([[
             this.__parent,'mousedown',
@@ -83,8 +113,8 @@ NEJ.define([
      * @return {Void}
      */
     _pro.__destroy = function(){
-        this.__doClearSort();
         this.__super();
+        this.__doClearSort();
     };
     /**
      * 取滚动条所在容器
@@ -112,6 +142,7 @@ NEJ.define([
             });
         };
         return function(){
+            this.__doUpdateThumb(3);
             _e._$removeByEC(this.__holder);
             _unSelect(this.__lsort,this.__selected);
             if (!!this.__auto){
@@ -121,6 +152,98 @@ NEJ.define([
             delete this.__lsort;
             delete this.__place;
             delete this.__offset;
+        };
+    })();
+    /**
+     * 更新缩略图
+     * @return {Void}
+     */
+    _pro.__doUpdateThumb = function(_flag,_pointer){
+        if (!this.__thumb) return;
+        // check thumb update
+        _pointer = _pointer||_o;
+        var _event = {
+            // 1 - first, 2 - doing, 3 - end
+            state:_flag, 
+            top:_pointer.y||0,
+            left:_pointer.x||0,
+            target:this.__thumb,
+            source:this.__lsort
+        };
+        this._$dispatchEvent(
+            'onthumbupdate',_event
+        );
+        // sort end
+        if (_flag==3){
+            _e._$removeByEC(this.__thumb);
+            return;
+        }
+        // user updated
+        document.body.appendChild(this.__thumb);
+        if (!!_event.stopped){
+            return;
+        }
+        // update position
+        _e._$style(
+            this.__thumb,{
+                top:_event.top+'px',
+                left:_event.left+'px'
+            }
+        );
+    };
+    /**
+     * 更新缩略图
+     * @return {Void}
+     */
+    _pro.__doUpdateHolder = (function(){
+        var _getOverBox = function(_from,_to){
+            var _offset = _e._$offset(_from,_to);
+            return {
+                top:_offset.y,
+                left:_offset.x,
+                width:_from.clientWidth,
+                height:_from.clientHeight
+            };
+        };
+        return function(_element,_pointer){
+            // calculate placeholder position
+            this.__parent.appendChild(this.__holder);
+            var _obox = _getOverBox(_element,this.__parent);
+            this.__place = this.__doCalPlaceHolder(
+                _obox,{
+                    top:_pointer.y-_obox.top-this.__offset.y,
+                    left:_pointer.x-_obox.left-this.__offset.x
+                }
+            );
+            this.__place.ref = _element;
+            this.__place.source = this.__lsort;
+            this.__place.target = this.__holder;
+            // check placeholder update
+            this._$dispatchEvent(
+                'onholderupdate',this.__place
+            );
+            if (!!this.__place.stopped){
+                return;
+            }
+            // update placeholder
+            _e._$style(
+                this.__holder,{
+                    top:this.__place.top+'px',
+                    left:this.__place.left+'px'
+                }
+            );
+            if (this.__place.width!=null){
+                _e._$setStyle(
+                    this.__holder,'width',
+                    this.__place.width+'px'
+                );
+            }
+            if (this.__place.height!=null){
+                _e._$setStyle(
+                    this.__holder,'height',
+                    this.__place.height+'px'
+                );
+            }
         };
     })();
     /**
@@ -178,6 +301,11 @@ NEJ.define([
         }
         // holder info
         this.__offset = _e._$offset(this.__parent);
+        this.__doUpdateThumb(1,{
+            x:_v._$clientX(_event),
+            y:_v._$clientY(_event)
+        });
+        // for auto scroll
         var _node = _u._$isArray(this.__lsort)
                   ? this.__lsort[0]:this.__lsort;
         this.__auto = _x._$$SmartScroll._$allocate({
@@ -192,59 +320,31 @@ NEJ.define([
      * @method module:util/sort/sortable._$$Sortable#__onSorting
      * @return {Void}
      */
-    _pro.__onSorting = (function(){
-        var _getOverBox = function(_from,_to){
-            var _offset = _e._$offset(_from,_to);
-            return {
-                top:_offset.y,
-                left:_offset.x,
-                width:_from.clientWidth,
-                height:_from.clientHeight
-            };
-        };
-        return function(_event){
-            if (!this.__lsort) return;
-            var _element = _v._$getElement(
-                _event,'c:'+this.__clazz
-            );
-            if (!_element) return;
-            // single selection
-            if (_element==this.__lsort){
-                delete this.__place;
-                _e._$removeByEC(this.__holder);
-                return;
+    _pro.__onSorting = function(_event){
+        if (!this.__lsort) return;
+        var _element = _v._$getElement(
+            _event,'c:'+this.__clazz
+        );
+        if (!_element) return;
+        // single selection
+        if (_element==this.__lsort){
+            delete this.__place;
+            _e._$removeByEC(this.__holder);
+            return;
+        }
+        // update thumbnail
+        this.__doUpdateThumb(2,{
+            x:_v._$clientX(_event),
+            y:_v._$clientY(_event)
+        });
+        // update placeholder
+        this.__doUpdateHolder(
+            _element,{
+                x:_v._$pageX(_event),
+                y:_v._$pageY(_event)
             }
-            // calculate placeholder position
-            this.__parent.appendChild(this.__holder);
-            var _obox = _getOverBox(_element,this.__parent);
-            this.__place = this.__doCalPlaceHolder(
-                _obox,{
-                    top:_v._$pageY(_event)-_obox.top-this.__offset.y,
-                    left:_v._$pageX(_event)-_obox.left-this.__offset.x
-                }
-            );
-            this.__place.ref = _element;
-            // update placeholder
-            _e._$style(
-                this.__holder,{
-                    top:this.__place.top+'px',
-                    left:this.__place.left+'px'
-                }
-            );
-            if (this.__place.width!=null){
-                _e._$setStyle(
-                    this.__holder,'width',
-                    this.__place.width+'px'
-                );
-            }
-            if (this.__place.height!=null){
-                _e._$setStyle(
-                    this.__holder,'height',
-                    this.__place.height+'px'
-                );
-            }
-        };
-    })();
+        );
+    };
     /**
      * 排序结束
      * 
