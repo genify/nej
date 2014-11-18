9a467a2279a0b31d4dce56b3bd59108f11ddfb8a
genify <genify@163.com>
2014-11-18 12:54:12 +0800

    bugfix for placeholder in ie9

--- src/util/placeholder/platform/holder.patch.js
+++ src/util/placeholder/platform/holder.patch.js
@@ -8,11 +8,11 @@
 NEJ.define([
     'base/element',
 	'base/event',
+	'base/util',
 	'./holder.js'
-],function(_e,_v,_h,_p,_o,_f,_r){
+],function(_e,_v,_u,_h,_p,_o,_f,_r){
 	// for ie9-
 	NEJ.patch('TR<=5.0',function(){
-		// variable declaration
 		/**
 		 * 节点占位符行为，高版本浏览器用样式处理
 		 * @param  {String|Node} _element 节点
@@ -21,27 +21,36 @@ NEJ.define([
 		 */
 		_h.__setPlaceholder = (function(){
 		    // placeholder flag
-		    var _cache = {};
+		    var _cache = {},
+		        _ropt = {
+		            nid:'j-holder-'+_u._$uniqueID()
+		        };
 		    // input foucs hide placeholder
-		    var _onFocus = function(_event){
-		        var _input = _v._$getElement(_event);
-		        _cache[_input.id] = 2;
+		    var _onFocus = function(_id){
+		        var _input = _e._$get(_id);
+		        _cache[_id] = 2;
 		        if (!!_input.value) return;
-		        _e._$setStyle(_e._$wrapInline(_input),'display','none');
+		        _e._$setStyle(
+		            _e._$wrapInline(_input,_ropt),
+		            'display','none'
+		        );
 		    };
 		    // input blur check placeholder show
-		    var _onBlur = function(_event){
-		        var _input = _v._$getElement(_event);
-		        _cache[_input.id] = 1;
+		    var _onBlur = function(_id){
+		        var _input = _e._$get(_id);
+		        _cache[_id] = 1;
 		        if (!!_input.value) return;
-		        _e._$setStyle(_e._$wrapInline(_input),'display','');
+		        _e._$setStyle(
+		            _e._$wrapInline(_input,_ropt),
+		            'display',''
+		        );
 		    };
 		    // input value change
-		    var _onInput = function(_event){
-		        var _input = _v._$getElement(_event);
-		        if (_cache[_input.id]==2) return;
+		    var _onInput = function(_id){
+		        var _input = _e._$get(_id);
+		        if (_cache[_id]==2) return;
 		        _e._$setStyle(
-		            _e._$wrapInline(_input),
+		            _e._$wrapInline(_input,_ropt),
 		            'display',!_input.value?'':'none'
 		        );
 		    };
@@ -50,7 +59,8 @@ NEJ.define([
 		        var _id = _e._$id(_input),
 		            _label = _e._$wrapInline(_input,{
 		                tag:'label',
-		                clazz:_clazz
+		                clazz:_clazz,
+		                nid:_ropt.nid
 		            });
 		        _label.htmlFor = _id;
 		        var _text = _e._$attr(_input,'placeholder')||'';
@@ -68,11 +78,12 @@ NEJ.define([
                 if (_cache[_input.id]!=null)
                     return;
                 _doWrapInput(_input,_clazz);
-                _cache[_input.id] = 1;
+		        var _id = _input.id;
+                _cache[_id] = 1;
                 // listen blur and focus event
-                _v._$addEvent(_input,'blur',_onBlur._$bind(null));
-                _v._$addEvent(_input,'focus',_onFocus._$bind(null));
-                _v._$addEvent(_input,'input',_onInput._$bind(null));
+                _v._$addEvent(_input,'blur',_onBlur._$bind(null,_id));
+                _v._$addEvent(_input,'focus',_onFocus._$bind(null,_id));
+                _v._$addEvent(_input,'input',_onInput._$bind(null,_id));
         	};
 		})();
     });
