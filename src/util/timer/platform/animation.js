0ea478ef32b4ccf86d2e86865ac59f05246d9e07
genify <genify@163.com>
2015-01-05 17:29:21 +0800

    use timeout for animation in android

--- src/util/timer/platform/animation.js
+++ src/util/timer/platform/animation.js
@@ -9,19 +9,29 @@ NEJ.define([
     'base/platform'
 ],function(_m,_p,_o,_f,_r){
     var _this = this,
-        _prefix = _m._$KERNEL.prefix.pro;
+        _prefix = _m._$KERNEL.prefix.pro,
+        _fps = _m._$is('desktop')?80:(_m._$is('ios')?50:30);
     /**
      * 请求动画
      * @param  {Function} 动画回调
      * @return {String}   动画标识
      */
     _p.__requestAnimationFrame = (function(){
-        var _handler = _this.requestAnimationFrame||
-                       _this[_prefix+'RequestAnimationFrame'];
+        var _handler = _m._$is('android')?null:(
+            _this.requestAnimationFrame||
+            _this[_prefix+'RequestAnimationFrame']
+        );
         return function(){
-            if (!!_handler){
-                return _handler.apply(this,arguments);
+            if (!_handler){
+                _handler = function(_callback){
+                    return window.setTimeout(
+                        function(){
+                            try{_callback(+new Date);}catch(ex){}
+                        },1000/_fps
+                    );
+                };
             }
+            return _handler.apply(this,arguments);
         };
     })();
     /**
@@ -30,12 +40,17 @@ NEJ.define([
      * @return {Void}
      */
     _p.__cancelAnimationFrame = (function(){
-        var _handler = _this.cancelAnimationFrame||
-                       _this[_prefix+'CancelAnimationFrame'];
+        var _handler = _m._$is('android')?null:(
+            _this.cancelAnimationFrame||
+            _this[_prefix+'CancelAnimationFrame']
+        );
         return function(){
-            if (!!_handler){
-                return _handler.apply(this,arguments);
+            if (!_handler){
+                _handler = function(_id){
+                    window.clearTimeout(_id);
+                };
             }
+            return _handler.apply(this,arguments);
         };
     })();
     
