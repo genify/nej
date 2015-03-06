3f827dea0865ce1bd6fec01be194dc7f6d79c2ae
java0racle <java0racle@163.com>
2015-03-06 14:29:00 +0800

    suggest test

--- src/util/suggest/test/suggest.test.js
+++ src/util/suggest/test/suggest.test.js
@@ -1,12 +1,13 @@
 var f = function(){
     module("suggest");
-    
+
     var _  = NEJ.P,
         _e = _('nej.e'),
         _v = _('nej.v'),
 		_p = _('nej.ut');
     test('suggest',function(){
 		stop();
+        expect(0);
 		var _ipt = _e._$get('suggest-input');
 		var _card0 = _e._$get('card0');
 		var _sg = _p._$$Suggest._$allocate({
@@ -16,14 +17,14 @@ var f = function(){
 			onchange:function(_value){
 				var _item = '';
 				for(var i = 1; i < 10;i++){
-				    _item += '<p>'+i+'</p>';
+				    _item += '<p data-value='+i+'>'+i+'</p>';
 				}
 				_card0.innerHTML = _item;
 				_sg._$setList(_e._$getChildren(_card0));
 			},
 			onselect:function(_value){
-				ok(true,'选择了一个值'+_value);
-				start();
+				// ok(true,'选择了一个值'+_value);
+				// start();
 			}
 		});
     });
@@ -33,4 +34,3 @@ test('define',function(){expect(0);
     define('{pro}suggest.test.js',['{lib}util/suggest/suggest.js'],f);
 });
 
-  
