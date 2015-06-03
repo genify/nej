0b5840d4e4a08ab011e1c622d37e7bc2ed4d0096
genify <genify@163.com>
2015-06-03 11:31:56 +0800

    demo for list module

--- src/util/list/demo/list/cache.js
+++ src/util/list/demo/list/cache.js
@@ -1,22 +1,26 @@
-var f = function(){
-	var _  = NEJ.P,
-	    _v = _('nej.v'),
-	    _t = _('nej.ut'),
-	    _p = _('tst.ut'),
-	    _proCustomCache;
-	
-	
-	_p._$$CustomCache = NEJ.C();
-	  _proCustomCache = _p._$$CustomCache._$extend(_t._$$AbstractListCache);
-	
-	
-	_proCustomCache.__doLoadList = function(_options){
-		
+NEJ.define([
+	'base/klass',
+	'base/event',
+	'util/event/event',
+	'util/cache/abstract'
+],function(_k,_v,_z,_t,_p,_pro){
+	/**
+	 *
+	 */
+	_p._$$CustomCache = _k._$klass();
+	_pro = _p._$$CustomCache._$extend(_t._$$CacheListAbstract);
+	/**
+	 *
+	 * @param _options
+	 * @private
+	 */
+	_pro.__doLoadList = function(_options){
+
 		// for test
 		var _arr = [],
-		    _total = 104,
-		    _count = Math.floor(_total/_options.limit),
-		    _number = _count*_options.limit;
+			_total = 104,
+			_count = Math.floor(_total/_options.limit),
+			_number = _count*_options.limit;
 		var _len = _options.offset>=_number?_total-_number:_options.limit;
 		for(var i=0;i<_total;i++){
 			_arr.push({id:+new Date+i,name:'user-'+(+new Date+i),loginTime:+new Date});
@@ -25,72 +29,75 @@ var f = function(){
 			this._$setTotal(_options.key,_total);
 		}
 		window.setTimeout(_options.onload._$bind(_options,_arr),1000);
-		
-	};
-    /**
-     * 从服务器上删除列表项，子类实现具体逻辑
-     * @protected
-     * @method {__doDeleteItem}
-     * @param   {Object}   请求信息
-     * @config  {String}   key      列表标识
-     * @config  {Number}   id       列表项标识
-     * @config  {String}   data     请求相关数据
-     * @config  {Function} onload   列表项载入回调
-     * @return {Void}
-     */
-    _proCustomCache.__doDeleteItem = function(_options){
-    	/*
-    	_j._$request(
-    	    '/rest/data/delete',{
-    	        // ...
-    	        onload:function(_json){
-    	            // remove from cache
-    	            var _event = _options.onload(!0);
-    	            // refresh list view
-                    _v._$dispatchEvent(
-                        _p._$$CustomCache,
-                        'listchange',_event
-                    );
-    	        }
-    	    }
-    	);
-    	*/
-    	// for test
-    	window.setTimeout(function(){
-    		var _event = _options.onload(!0);
-    		_v._$dispatchEvent(
-    			_p._$$CustomCache,'listchange',_event);
-    	},500);
-    };
 
-    /**
-     * 从服务器上删除列表项，子类实现具体逻辑
-     * @protected
-     * @method {__doDeleteItem}
-     * @param   {Object}   请求信息
-     * @config  {String}   key      列表标识
-     * @config  {Number}   id       列表项标识
-     * @config  {String}   data     请求相关数据
-     * @config  {Function} onload   列表项载入回调
-     * @return {Void}
-     */
-    _proCustomCache.__doUpdateItem = function(_options){
-        
-        // for test
-        window.setTimeout(function(){
-            var _event = _options.onload(_options.data);
-            //_v._$dispatchEvent(
-            //    _p._$$CustomCache,'listchange',_event);
-        },500);
-        
-    };
-	
-	_t._$$CustomEvent._$allocate({
+	};
+	/**
+	 * 从服务器上删除列表项，子类实现具体逻辑
+	 * @protected
+	 * @method {__doDeleteItem}
+	 * @param   {Object}   请求信息
+	 * @config  {String}   key      列表标识
+	 * @config  {Number}   id       列表项标识
+	 * @config  {String}   data     请求相关数据
+	 * @config  {Function} onload   列表项载入回调
+	 * @return {Void}
+	 */
+	_pro.__doDeleteItem = function(_options){
+		/*
+		 _j._$request(
+		 '/rest/data/delete',{
+		 // ...
+		 onload:function(_json){
+		 // remove from cache
+		 var _event = _options.onload(!0);
+		 // refresh list view
+		 _v._$dispatchEvent(
+		 _p._$$CustomCache,
+		 'listchange',_event
+		 );
+		 }
+		 }
+		 );
+		 */
+		// for test
+		window.setTimeout(function(){
+			var _event = _options.onload(!0);
+			_v._$dispatchEvent(
+				_p._$$CustomCache,
+				'listchange',_event
+			);
+		},500);
+	};
+
+	/**
+	 * 从服务器上删除列表项，子类实现具体逻辑
+	 * @protected
+	 * @method {__doDeleteItem}
+	 * @param   {Object}   请求信息
+	 * @config  {String}   key      列表标识
+	 * @config  {Number}   id       列表项标识
+	 * @config  {String}   data     请求相关数据
+	 * @config  {Function} onload   列表项载入回调
+	 * @return {Void}
+	 */
+	_pro.__doUpdateItem = function(_options){
+
+		// for test
+		window.setTimeout(function(){
+			_options.data.name = 'up-'+(+new Date);
+			var _event = _options.onload(_options.data);
+			_v._$dispatchEvent(
+			    _p._$$CustomCache,
+				'listchange',_event
+			);
+		},500);
+
+	};
+
+	_z._$$CustomEvent._$allocate({
 		element:_p._$$CustomCache,
 		event:'listchange'
 	});
-	
-};
-define('{pro}cache.js',
-      ['{lib}util/event/event.js'
-      ,'{lib}util/cache/cache.list.base.js'],f)
+
+	return _p;
+});
--- src/util/list/demo/list/item.js
+++ src/util/list/demo/list/item.js
@@ -1,41 +1,67 @@
-var f = function(){
-    var _  = NEJ.P,
-        _o = NEJ.O,
-        _f = NEJ.F,
-        _e = _('nej.e'),
-        _v = _('nej.v'),
-        _u = _('nej.u'),
-        _i = _('nej.ui'),
-        _p = _('tst.ut'),
-        _proItem,
-        _supItem;
-        
-        
+NEJ.define([
+    'base/klass',
+    'base/util',
+    'base/event',
+    'base/element',
+    'ui/item/list',
+    'util/template/tpl',
+    'text!./ntp.html'
+],function(_k,_u,_v,_e,_i,_t,_html,_p,_pro){
+    /**
+     *
+     */
     _p._$$Item = NEJ.C();
-      _proItem = _p._$$Item._$extend(_i._$$ListItem);
-    
-    _proItem.__reset = function(_options){
-        this.__supReset(_options);
+    _pro = _p._$$Item._$extend(_i._$$ListItem);
+    /**
+     *
+     * @param _options
+     * @private
+     */
+    _pro.__reset = function(_options){
+        this.__super(_options);
         this.__doInitDomEvent(this.__xxev);
     };
     /**
      * 初始化模版
      */
-    _proItem.__initXGui = function() {
-        this.__seed_html = 'ntp-item';
+    _pro.__initXGui = (function(){
+        var _seed = _t._$addNodeTemplate(_html);
+        return function() {
+            this.__seed_html = _seed;
+        };
+    })();
+    /**
+     *
+     */
+    _pro.__initNode = function(){
+        this.__super();
+        var _list = _e._$getByClassName(
+            this.__body,'js-flag'
+        );
+        this.__nbox = _list[2];
+        this.__xxev = [[
+            _list[0],'click',this.__onDelete._$bind(this)
+        ],[
+            _list[1],'click',this.__onUpdate._$bind(this)
+        ]];
     };
     /**
-     * 
+     *
+     * @param _event
+     * @private
      */
-    _proItem.__initNode = function(){
-    	this.__supInitNode();
-    	var _list = _e._$getByClassName(this.__body,'js-flag');
-    	this.__nbox = _list[2];
-    	this.__xxev = [[
-    	    _list[0],'click',this.__onDelete._$bind(this)
-    	],[
-    	    _list[1],'click',this.__onUpdate._$bind(this)
-    	]];
+    _pro.__onDelete = function(_event){
+        _v._$stop(_event);
+        this.__super();
+    };
+    /**
+     *
+     * @param _event
+     * @private
+     */
+    _pro.__onUpdate = function(_event){
+        _v._$stop(_event);
+        this.__super();
     };
     /**
      * 刷新项,子类实现具体逻辑
@@ -43,8 +69,9 @@ var f = function(){
      * @method {__doRefresh}
      * @return {Void}
      */
-    _proItem.__doRefresh = function(_data){
-    	this.__nbox.innerText = this.__index+'. '+_data.name+'：'+_u._$format(_data.loginTime,'yyyy-MM-dd HH:mm:ss');
+    _pro.__doRefresh = function(_data){
+        this.__nbox.innerText =
+            this.__index+'. '+_data.name+'：'+
+            _u._$format(_data.loginTime,'yyyy-MM-dd HH:mm:ss');
     };
-};
-define('{pro}item.js',['{lib}ui/item/list.js'],f);
+});
