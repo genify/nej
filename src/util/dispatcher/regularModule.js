0e686ebee70724403b12fe6b6058941466fd6671
Yusen <rainforest92@126.com>
2015-12-08 17:41:22 +0800

    add CHANGELOG

--- src/util/dispatcher/regularModule.js
+++ src/util/dispatcher/regularModule.js
@@ -20,7 +20,13 @@ NEJ.define([
      */
     _p._$$RegularModule = _k._$klass();
     _pro = _p._$$RegularModule._$extend(_m._$$ModuleAbstract);
-
+    /**
+     * 控件初始化
+     * 
+     * @protected
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__init
+     * @return {Void}
+     */
     _pro.__init = function(){
         this.__super();
         this.__nodeKey = _t._$addNodeTemplate('<div></div>');
@@ -28,7 +34,14 @@ NEJ.define([
         this._$innerModule = new this._$$InnerModule().$inject(this.__body);
         this.__export.parent = this._$innerModule.$refs.view;
     }
-
+    /**
+     * 显示模块触发事件，子类可重写具体逻辑
+     * 
+     * @protected
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__onShow
+     * @param  {Object} arg0 - 事件对象
+     * @return {Void}
+     */
     _pro.__onShow = function(_options){
         this.__super(_options);
         if(this._$innerModule.__onShow){
@@ -38,7 +51,14 @@ NEJ.define([
             this._$innerModule.$update();
         }
     }
-
+    /**
+     * 显示模块触发事件，子类实现具体逻辑
+     * 
+     * @protected
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__onRefresh
+     * @param  {Object} arg0 - 事件对象
+     * @return {Void}
+     */
     _pro.__onRefresh = function(_options){
         this.__super(_options);
         if(this._$innerModule.__onRefresh){
@@ -48,7 +68,14 @@ NEJ.define([
             this._$innerModule.$update();
         }
     }
-
+    /**
+     * 接受到消息触发事件，子类实现具体逻辑
+     * 
+     * @abstract
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__onMessage
+     * @param  {Object} arg0 - 事件对象
+     * @return {Void}
+     */
     _pro.__onMessage = function(_options){
         this.__super(_options);
         if(this._$innerModule.__onMessage){
@@ -56,7 +83,14 @@ NEJ.define([
             this._$innerModule.$update();
         }
     }
-
+    /**
+     * 模块退出前触发事件，通过阻止输入的事件做退出验证，子类实现具体逻辑
+     * 
+     * @abstract
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__onBeforeHide
+     * @param  {Object} arg0 - 事件对象
+     * @return {Void}
+     */
     _pro.__onBeforeHide = function(_options){
         this.__super(_options);
         if(this._$innerModule.__onBeforeHide){
@@ -64,7 +98,13 @@ NEJ.define([
             this._$innerModule.$update();
         }
     }
-
+    /**
+     * 隐藏模块触发事件，子类实现具体逻辑
+     * 
+     * @protected
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__onHide
+     * @return {Void}
+     */
     _pro.__onHide = function(){
         this.__super();
         if(this._$innerModule.__onHide){
@@ -72,7 +112,13 @@ NEJ.define([
             this._$innerModule.$update();
         }
     }
-
+    /**
+     * 控件销毁
+     * 
+     * @protected
+     * @method module:util/dispatcher/regularModule._$$RegularModule#__destroy
+     * @return {Void}
+     */
     _pro.__destroy = function(){
         this.__body = _t._$getNodeTemplate(this.__nodeKey);
         this._$innerModule.destroy();
