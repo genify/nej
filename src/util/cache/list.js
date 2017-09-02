306a00886b0312c9f07c63901b35cc4de1c3a5fe
caijf <caijf@corp.netease.com>
2017-09-02 11:43:19 +0800

    support batch items add for list cache

--- src/util/cache/list.js
+++ src/util/cache/list.js
@@ -932,24 +932,40 @@ NEJ.define([
      * @return {Void}
      */
     _pro.__addItem = function(_options,_item){
-        var _key = _options.key;
-        _item = this.__doSaveItemToCache(_item,_key);
-        // add to list
-        if (!!_item){
-            var _flag = 0,
-                _list = this._$getListInCache(_key);
-            if (!_options.push){
-                _flag = -1;
-                var _offset = _options.offset||0;
-                _list.splice(_offset,0,_item);
-            }else if(_list.loaded){
-                _flag = 1;
-                _list.push(_item);
-            }else{
-                // add total
-                _list.length++;
+        var _key = _options.key,
+            _flag = 0;
+        // add one item to cache
+        var addOneItem = function(item){
+            var _item = this.__doSaveItemToCache(item,_key);
+            // add to list
+            if (!!_item){
+                var _list = this._$getListInCache(_key);
+                if (!_options.push){
+                    _flag = -1;
+                    var _offset = _options.offset||0;
+                    _list.splice(_offset,0,_item);
+                }else if(_list.loaded){
+                    _flag = 1;
+                    _list.push(_item);
+                }else{
+                    // add total
+                    _list.length++;
+                }
             }
+            return _item;
+        };
+
+        // check batch add
+        if (_u._$isArray(_item)){
+            var ret = [];
+            _u._$forEach(_item,function(it){
+                ret.push(addOneItem(it));
+            });
+            _item = ret;
+        }else{
+            _item = addOneItem(_item);
         }
+
         // callback
         var _event = {
             key:_key,
