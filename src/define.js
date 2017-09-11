0e36e839d9b82b5e9a023de3519a0c264aea3521
caijf <caijf@corp.netease.com>
2017-09-11 23:19:15 +0800

    ignore nej script for NEJ.define

--- src/define.js
+++ src/define.js
@@ -539,10 +539,16 @@
             var _code = _script.innerHTML;
             return _code.search(_reg)>=0;
         };
+        var _isNEJTemplate = function(type){
+            return (type||'').search(/^nej\//i)===0;
+        };
         return function(){
             var _list = d.getElementsByTagName('script');
             for(var i=_list.length-1,_script;i>=0;i--){
                 _script = _list[i];
+                if (_isNEJTemplate(_script.type)){
+                    continue;
+                }
                 if (!_script.xxx){
                     _script.xxx = !0;
                     if (!_script.src&&_isNEJInline(_script)){
